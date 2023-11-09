c7ae35f5fc9ee5277d637a990b40a3d6e5e4223a
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2023-11-10 03:13:39 +0900

    不要な差分

--- lib/poke_repository.dart
+++ lib/poke_repository.dart
@@ -1,7 +1,4 @@
 import 'dart:convert';
-
-import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 
